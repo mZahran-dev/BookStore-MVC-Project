6b6227e4d675f5d16e12f2ad2b1283857fb9e900
Mahmoud Zahran <mi4654110@gmail.com>
2024-09-27 02:02:53 +0300

    Add project files.

---
+++ BookStore.PL/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookStore.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
