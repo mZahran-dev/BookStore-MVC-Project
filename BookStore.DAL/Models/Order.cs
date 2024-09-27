039607b3a41018eb12ef24e721adc7bd1af61a03
Mahmoud Zahran <mi4654110@gmail.com>
2024-09-27 06:36:32 +0300

    feat : Implement Order Class

--- BookStore.DAL/Models/Order.cs
+++ BookStore.DAL/Models/Order.cs
@@ -8,5 +8,9 @@ namespace BookStore.DAL.Models
 {
     public class Order
     {
+        public int Id { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new();
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
     }
 }
