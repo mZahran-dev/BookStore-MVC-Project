e83a77151723542406fe62cebead58df7c1af707
Mahmoud Zahran <mi4654110@gmail.com>
2024-09-27 06:22:42 +0300

    feat : Implement Book Class

--- BookStore.DAL/Models/Book.cs
+++ BookStore.DAL/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,25 @@ namespace BookStore.DAL.Models
 {
     public class Book
     {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [MaxLength(100)]
+        public string Description { get; set; }
+        public string Language { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Display(Name ="Date Of Published")]
+        public DateTime DatePublished { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Image URL")]
+        public string ImageUrl  { get; set; }
+        
     }
 }
