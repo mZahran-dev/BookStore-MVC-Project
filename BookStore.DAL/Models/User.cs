fcc442b930721bb4c2117ffc05abb69c337fc220
Mahmoud Zahran <mi4654110@gmail.com>
2024-09-27 06:32:32 +0300

    feat : implement User Class

--- BookStore.DAL/Models/User.cs
+++ BookStore.DAL/Models/User.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BookStore.DAL.Models
 {
-    internal class User
+    public class User : IdentityUser
     {
+        [PersonalData]
+        [Required]
+        public string FName { get; set; }
+        [PersonalData]
+        [Required]
+        public string LName { get; set; }
+        [PersonalData]
+        [Required]
+        public string Address { get; set; }
+        [PersonalData]
+        [Required]
+        public int ZipCode  { get; set; }
+        [PersonalData]
+        [DataType(DataType.Date)]
+        public DateTime UserCreationDate { get; set; } = DateTime.Now;
+
+
+
     }
 }
